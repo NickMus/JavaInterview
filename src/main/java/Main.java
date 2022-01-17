7ef06edee1bc63410d9f212f358161201161362f
nmusi <n.musinov@gmail.com>
2022-01-17 11:53:34 +0300

    hw1

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person.Builder()
+                .firstName("ivan")
+                .lastName("ivanov")
+                .age(30)
+                .country("russia")
+                .address("moscow")
+                .phone("123467")
+                .build();
+
+    }
+}
