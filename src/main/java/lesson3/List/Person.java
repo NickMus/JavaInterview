8a5694fdf22483ad65cd7dd6b0a9d1daab832a9a
nmusi <n.musinov@gmail.com>
2022-01-24 13:07:55 +0300

    hw3

---
+++ src/main/java/lesson3/List/Person.java
@@ -0,0 +1,19 @@
+package lesson3.List;
+
+public class Person {
+    private String name;
+    private String age;
+
+    public Person(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
